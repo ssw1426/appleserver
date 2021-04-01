c3f965aecf3bf59f233f002af16c474226a8dd7a
ssw1426 <80308398+ssw1426@users.noreply.github.com>
2021-04-01 21:06:47 +0900

    Update dmsend.py

--- dmsend.py
+++ dmsend.py
@@ -34,5 +34,5 @@ async def on_message(message):
                 except:
                     pass
 
-access_token = os.environ['BOT_TOKEN]
+access_token = os.environ["BOT_TOKEN"]
 client.run(access_token)
